6caaf78533a3d8c8912ac247c992d414808f20b8
prajjvaljaiswal <prajjaiswal158@gmail.com>
2024-09-07 22:47:14 +0530

    - 🚀 After email validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
+import { Validation } from './Validation';
 
 export const Login = () => {
 
-    const [isLogin,setIsLogin] = useState(false);  
+    const [isLogin,setIsLogin] = useState(true);  
+    const email = useRef(null)
+    const password = useRef(null)
+    const [errMessage,setErrMessage] = useState(null) 
 
     function toggleSignInForm(){
         setIsLogin(!isLogin);
+    }
+
+    function handelButtonClick(){
+        const message = Validation(email.current.value,password.current.value)
+        setErrMessage(message)
     }
 
   return (
@@ -16,21 +25,27 @@ export const Login = () => {
             <img src='https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg'
             alt='Bg' />
         </div>
-        <form className='absolute bg-black my-36 w-3/12 p-12 mx-auto right-0 left-0 text-white
+        <form onSubmit={(e)=>{return e.preventDefault()}} className='absolute bg-black my-36 w-3/12 p-12 mx-auto right-0 left-0 text-white
         bg-opacity-80 rounded-lg'>
 
             <h1 className='text-3xl font-bold py-4'>{isLogin?'Sign-in':"Sign-up"}</h1>
-            <input type='text' placeholder='email' className='p-2 my-4 w-full bg-black bg-opacity-50'/>
+            <input ref={email} type='text' placeholder='email' className='p-2 my-4 w-full bg-black bg-opacity-50'/>
             {
                 (!isLogin && <input type='text' placeholder='username' className='p-2 my-4 w-full bg-black bg-opacity-50'/>)
             }
-            <input type='password' placeholder='password' className='p-2 my-4 w-full bg-black bg-opacity-50'/>
+            <input ref={password} type='password' placeholder='password' className='p-2 my-4 w-full bg-black bg-opacity-50'/>
+            
+            <p className='py-4 text-red-700'>
+                {errMessage}
+            </p>
+
+            <button className='p-4 my-6 w-full bg-red-600 rounded-lg' onClick={handelButtonClick}>
+                {isLogin?'Sign-in':"Sign-up"}
+            </button>
+
             <h1 className='py-4 cursor-pointer' onClick={toggleSignInForm}>
                 {isLogin?"New to Netflix? Sign up now":"Alredy Signed up? Sign in now"}
             </h1>
-            <button className='p-4 my-6 w-full bg-red-600 rounded-lg'>
-                {isLogin?'Sign-in':"Sign-up"}
-            </button>
 
         </form>
     </div>
--- src/components/Validation.js
+++ src/components/Validation.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export const Validation = (email,password) => {
 
-    
+    const isEmailVaild = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/.test(email)
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
 
+    if(!isEmailVaild){
+        return "Email is not valid"
+    }
 
-    return true;
+    if(!isPasswordValid){
+        return "Password is not valid"
+    }
+
+    return null;
 }
